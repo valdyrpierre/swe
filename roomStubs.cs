c5f8fcaccb31f5d6f02ba4a0bc6800fc590bef1a
valdyrpierre <vpierre509@gmail.com>
2025-09-30 10:35:42 -0400

    Add files via upload

---
+++ roomStubs.cs
@@ -0,0 +1,40 @@
+// this will get the information about the room
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Room_Class()
+{
+
+    public string roomName;
+    public int roomCapacity;
+
+    public int setRoomCapacity(int roomCapacity)
+    {
+        // this will have the user put in the room capacity
+        // i use int to make sure it is only num
+        // i will have it keep looping until they put in a valid num
+    }
+
+    public int getRoomCapacity()
+    {
+        // ths will dispalay the room capacity
+
+    }
+
+    public string setRoomName(string roomName)
+    {
+        // this will have the user put in the room name
+        // i use string to make sure it is only letters
+        // i will have it keep looping until they put in a valid name
+    }
+
+    public string getRoomName()
+    {
+        // this will display the room name
+        
+    }
+
+}
