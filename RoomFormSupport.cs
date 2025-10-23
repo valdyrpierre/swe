c91d86e51fe7c20eec8f9ded96cc7292310dbc73
Curry-Ops <wheldon79@gmail.com>
2025-10-23 11:02:10 -0400

    Add files via upload
    these are the classes that Doc made in class.

---
+++ RoomFormSupport.cs
@@ -0,0 +1,30 @@
+﻿using CapstoneTableSide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone
+
+{
+    public class RoomFormSupport
+
+    {
+        CapstoneTableSide.RoomTableSupport rts = new CapstoneTableSide.RoomTableSupport();
+        public RoomFormSupport() { }
+
+
+            public void populateDataBase(List<Roomt> r)
+
+        {
+            rts.populateDataBase(r);
+        }
+
+        public List<Roomt> populateRoomForm()
+        {
+            return rts.retrieveRooms();
+        }
+
+    }
+}
---
+++ RoomTableSupport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneTableSide
+{
+    public class RoomTableSupport
+    {
+        //attribute to connect to database
+        public RoomTableSupport() { }
+
+
+        public void populateDataBase(List<Room> r)
+
+        {
+            //code to populate database with list of rooms
+        }
+
+        public List<Room> retrieveRooms()
+        {
+            //code to retrieve list of rooms from database and return it
+        }
+        
+    }
+}
